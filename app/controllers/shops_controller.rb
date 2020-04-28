d2a0d2dc9efa21bedabc5c95fc00171ca4955d93
G-Shino <37409914+G-Shino@users.noreply.github.com>
2020-04-28 05:20:40 +0000

    add shop controller and view

---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,37 @@
+class ShopsController < ApplicationController
+  def index
+    @shops = Shop.all
+  end
+  
+  def show
+    @shop=Shop.find(params[:id])
+  end
+
+  def new
+    @shop = current_user.shops.build
+  end
+
+  def create
+    @shop = current_user.shops.build(shop_params)
+    if @shop.save
+      flash[:success] = "店舗を作成しました"
+      redirect_to @shop
+    else
+      flash.now[:danger] = "店舗作成に失敗しました"
+      render :new 
+    end
+  end
+
+  def destroy
+    @shop = Shop.find(params[:id])
+    @shop.destroy
+    flash[:success] = "店舗を削除しました"
+    redirect_to shops_path
+  end
+  
+  private
+  
+  def shop_params
+    params.require(:shop).permit(:name, :kind, :address, :holiday, :image)
+  end
+end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,10 @@
 class Shop < ApplicationRecord
+  validates :name, presence: true
+  validates :kind, presence: true
+  validates :address, presence: true
+  validates :holiday, presence: true
+  validates :image, presence: true
+ 
   belongs_to :user
+  has_one_attached :image
 end
